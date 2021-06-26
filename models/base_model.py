817046c8730c913183b7b9cdafb2526cbfd37a89
Juansu01 <thejuank0@hotmail.com>
2021-06-26 12:27:47 -0500

    Updated init method with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,18 @@ class BaseModel:
         None
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializator method """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs and not args:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            del kwargs["__class__"]
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """ Print class format """
