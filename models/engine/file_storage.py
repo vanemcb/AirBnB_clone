5ca7d980adaeb013dba4423bc4cbb32e2c2b3b51
vanemcb <vanem2201@gmail.com>
2021-06-27 15:56:37 -0500

    Deserialization fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,14 +3,8 @@
     This module is for the project 0x00. AirBnB clone - The console
     proposed by Holberton school to learn how a web page works.
 """
-
-"""from datetime import datetime
-import uuid
-import os, sys
-p = os.path.abspath('..')
-sys.path.insert(1, p)
-from models.base_model import BaseModel"""
 import json
+
 
 class FileStorage:
     """FileStorage class that serializes instances to a JSON file
@@ -25,27 +19,31 @@ class FileStorage:
 
     def all(self):
         """ returns the dictionary __objects """
-        from models.base_model import BaseModel
-        my_dict_copy = FileStorage.__objects.copy()
-        for obj_key, obj_value in FileStorage.__objects.items():
-            my_dict_copy[obj_key] = BaseModel(**obj_value)
-        return my_dict_copy
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
-        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj.to_dict()
+        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
+        from models.base_model import BaseModel
         with open(FileStorage.__file_path, "w", encoding='utf-8') as my_file:
-            my_file.write(json.dumps(FileStorage.__objects))
+            my_dict_copy = FileStorage.__objects.copy()
+
+            for obj_key, obj_value in FileStorage.__objects.items():
+                my_dict_copy[obj_key] = obj_value.to_dict()
+            my_file.write(json.dumps(my_dict_copy))
 
     def reload(self):
         """ Deserializes the JSON file to __objects  """
         from models.base_model import BaseModel
         try:
-            with open(FileStorage.__file_path, "r", encoding='utf-8') as my_file2:
-                FileStorage.__objects = json.loads(my_file2.read())
+            with open(FileStorage.__file_path, "r") as my_file2:
+                my_dict = json.loads(my_file2.read())
+
+            for obj_key, obj_value in my_dict.items():
+                FileStorage.__objects[obj_key] = BaseModel(**obj_value)
 
         except:
             pass
