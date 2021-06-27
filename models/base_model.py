3b88ab0e38b831ee462e30057ce40e08ce054bb9
vanemcb <vanem2201@gmail.com>
2021-06-27 11:16:14 -0500

    __init__ and save method updated

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 import uuid
 from datetime import datetime
+from __init__ import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new()
         else:
             del kwargs["__class__"]
             kwargs["created_at"] = datetime.strptime(
@@ -41,6 +43,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values
