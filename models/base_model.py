32565d41be1063ce3aa81a409cd0d15d449b7951
vanemcb <vanem2201@gmail.com>
2021-06-29 18:56:29 -0500

    Class modified

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initializator method """
-        if not kwargs and not args:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
