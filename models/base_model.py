8b0351f3668c917e4c8c37d6b7adcea712a34265
vanemcb <vanem2201@gmail.com>
2021-06-29 20:02:48 -0500

    Init condition modified

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initializator method """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
+        if kwargs:
             del kwargs["__class__"]
             kwargs["created_at"] = datetime.strptime(
                 kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
@@ -32,6 +27,11 @@ class BaseModel:
                 kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
                 setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ Print class format """
