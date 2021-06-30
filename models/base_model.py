4d53bb777315d0661ff546f106ea8b257db5b6e3
vanemcb <vanem2201@gmail.com>
2021-06-29 20:20:24 -0500

    Change setattr for update

--- models/base_model.py
+++ models/base_model.py
@@ -25,8 +25,7 @@ class BaseModel:
                 kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
             kwargs["updated_at"] = datetime.strptime(
                 kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            self.__dict__.update(kwargs)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
